58f1e04a33e0a5a0d1ed1df0e6da183abcb9dae7
jinzhijie <742905606@qq.com>
2020-03-31 16:07:59 +0800

    在开始前检查密钥对

--- RSA+Base64.py
+++ RSA+Base64.py
@@ -24,6 +24,22 @@ def findpem():
     return keylist
 
 
+def check_self_pem():  # 检查并生成
+    exist_pri = os.path.exists('private.pem')
+    exist_pub = os.path.exists('public.pem')
+    if not exist_pri:
+        print('未找到私钥')
+    if not exist_pub:
+        print('未找到公钥')
+    if not exist_pri and not exist_pri:
+        if input("是否生成新的密钥对(Y/N) >>>").lower() == "y":
+            import generatekeys
+        else:
+            e = '无法找到密钥对'
+            raise Exception(e)
+
+
+check_self_pem()  # 先判断是否有公钥
 PublicKeyList = findpem()
 
 with open('private.pem', "rb") as privatefile:  # 加载自己的密钥
@@ -31,27 +47,31 @@ with open('private.pem', "rb") as privatefile:  # 加载自己的密钥
     privkey = rsa.PrivateKey.load_pkcs1(p)
 
 
-
 while True:
-    Mode = int(input("选择模式(0:解密, 1:加密)>>>"))
+    Mode = int(input("选择模式：[0] 解密, [1] 加密) >>>"))
     if Mode:
         for index in range(len(PublicKeyList)):
-            print('[{index}] {name}'.format(index=index, name=PublicKeyList[index]['name']))
-        index = int(input("请选择收信人>>>"))
+            print('[{index}] {name}'.format(
+                index=index, name=PublicKeyList[index]['name']))
+        index = int(input("请选择收信人 >>>"))
         with open(PublicKeyList[index]['path'], "rb") as thirdfile:  # 加载 别人的公钥
             p = thirdfile.read()
             third = rsa.PublicKey.load_pkcs1(p)
         message = input('Message >>>')
-        ciphertext = prefix_m + base64.b64encode(rsa.encrypt(message.encode('utf-8'), third)) + suffix_m
-        if input("是否签名(Y/N)>>>").lower() == "y":
-            signature = base64.b64encode(rsa.sign(message.encode('utf-8'), privkey, encryption_method))
+        ciphertext = prefix_m + \
+            base64.b64encode(rsa.encrypt(
+                message.encode('utf-8'), third)) + suffix_m
+        if input("是否签名(Y/N) >>>").lower() == "y":
+            signature = base64.b64encode(
+                rsa.sign(message.encode('utf-8'), privkey, encryption_method))
             ciphertext = ciphertext + prefix_s + signature + suffix_s
         with open('result.rsa', 'wb') as resultfile:
             resultfile.write(ciphertext)
         print('已将密文输出至 result.rsa')
     else:
         for index in range(len(PublicKeyList)):
-            print('[{index}] {name}'.format(index=index, name=PublicKeyList[index]['name']))
+            print('[{index}] {name}'.format(
+                index=index, name=PublicKeyList[index]['name']))
         index = int(input("请选择发件人>>>"))
         with open(PublicKeyList[index]['path'], "rb") as thirdfile:  # 加载 别人的公钥
             p = thirdfile.read()
@@ -62,7 +82,7 @@ while True:
         try:
             message = rsa.decrypt(crypto, privkey)
         except rsa.pkcs1.DecryptionError:
-            print("这不是给你的密文")
+            print("无法解密，这可能不是给你的密文")
             input("回车重新开始")
             os.system('cls')
             continue
@@ -71,16 +91,18 @@ while True:
         if len(c) >= 5:
             signature = base64.b64decode(c[4])  # 签名部分sign 用私钥签名认证、再用公钥验证签名
             try:
-                method_name = rsa.verify(message.encode('utf-8'), signature, third)
+                method_name = rsa.verify(
+                    message.encode('utf-8'), signature, third)
             except rsa.pkcs1.VerificationError:
-                print("×签名无效")
+                print("× 签名无效")
             else:
                 if method_name == encryption_method:
                     print('√ 签名有效 [%s]' % method_name)
             finally:
-                print("信息为："+message)
+                print("信息为："+ message)
         else:
-            print('x没有签名')
-            print("信息为："+message)
+            print('× 没有签名')
+            print("信息为："+ message)
+    
     input("回车重新开始")
     os.system('cls')
