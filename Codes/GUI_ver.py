fe11b7d1d0bf46df652f9b1bd556bad6c7cce87c
CharlieYu4994 <charlieyu4994@outlook.com>
2020-07-11 19:45:41 +0800

    更新

--- Codes/GUI_ver.py
+++ Codes/GUI_ver.py
@@ -1,32 +1,90 @@
-from tkinter import Button, Checkbutton, Frame, Label, Tk, scrolledtext
-from tkinter.ttk import Combobox
+import tkinter
+from tkinter import ttk
+from tkinter import scrolledtext
 
-window = Tk()
-window.title('RSA&AES Encryption')
-window.geometry('398x200')
-window.resizable(0,0) 
+class cfgwindow(tkinter.Toplevel):
+    def __init__(self):
+        super().__init__()
+        self.title('')
+        self.geometry('200x200')
+        self.resizable(0,0)
+        self.setup_UI()
+    def setup_UI(self):
+        pass
 
-inputbox = scrolledtext.ScrolledText(window, width=54, height=10)
-inputbox.grid(column=0, row=0)
+class MainLoop(tkinter.Tk):
+    keylist = [1]
 
-actionbox = Frame(window)
-actionbox.grid(column=0, row=1)
+    def __init__(self):
+        super().__init__()
+        self.title('RSA&AES Encryption')
+        self.geometry('372x205')
+        self.resizable(0,0)
+        self.setupUI()
 
-space = Label(actionbox)
-space.grid(columnspan=3, row=0)
+    def setupUI(self):
+        tabs = ttk.Notebook(self)
 
-cfgbtn = Button(actionbox, text='修改配置', relief='groove')
-cfgbtn.grid(column=2, row=1)
+#--------------------------------------------第一页------------------------------------------------#
+        frame0 = ttk.Frame(tabs)
 
-encryptbtn = Button(actionbox, text='加密', relief='groove')
-encryptbtn.grid(column=3, row=1)
+        inputbox = scrolledtext.ScrolledText(frame0, width=50, height=10)
+        inputbox.grid(column=0, row=0)
 
-signcheck = Checkbutton(actionbox, text = "是否签名")
-signcheck.grid(column=1, row=1)
+        footbox0 = ttk.Frame(frame0)
+        footbox0.grid(column=0, row=1, pady=10)
 
-combo = Combobox(actionbox)
-combo['values'] = [1]
-combo.current(0)
-combo.grid(column=0, row=1)
+        signcheck = ttk.Checkbutton(footbox0, text = "签名")
+        signcheck.grid(column=5, row=0, padx=20)
 
-window.mainloop()
+        rightbox0 = ttk.Frame(footbox0)
+        rightbox0.grid(column=6, row=0)
+
+        encryptbtn_t = ttk.Button(rightbox0, width=8, text='加密')
+        encryptbtn_t.grid(column=0, row=0)
+
+        decryptbtn_t = ttk.Button(rightbox0, width=8, text='解密')
+        decryptbtn_t.grid(column=1, row=0)
+
+#--------------------------------------------第二页------------------------------------------------#
+        frame1 = ttk.Frame(tabs)
+        
+        rightbox1 = ttk.Frame(frame1)
+        rightbox1.grid(column=1, row=0)
+
+        encryptbtn_f = ttk.Button(rightbox1, width=8, text='加密')
+        encryptbtn_f.grid(column=0, row=0)
+
+        decryptbtn_f = ttk.Button(rightbox1, width=8, text='解密')
+        decryptbtn_f.grid(column=1, row=0)
+
+#--------------------------------------------第三页------------------------------------------------#
+        frame2 = ttk.Frame(tabs)
+
+#--------------------------------------------标签栏------------------------------------------------#
+        keybox = ttk.Frame(self)
+        keybox.grid(column=0, row=0, sticky = tkinter.NE, padx=5, pady=1)
+
+        prompt = ttk.Label(keybox, text="收件人:")
+        prompt.grid(column=0, row=0, sticky = tkinter.W)
+
+        keyls = ttk.Combobox(keybox, width=10)
+        keyls['values'] = self.keylist
+        keyls.current(0)
+        keyls.grid(column=1, row=0)
+
+        tabs.add(frame0, text="文本加/解密")
+        tabs.add(frame1, text="文件加/解密")
+        tabs.add(frame2, text="设置")
+        tabs.grid(column=0, row=0)
+
+    def change_cfg(self):
+        subwindow = cfgwindow()
+        subwindow.focus_set()
+        self.wait_window(subwindow)
+
+
+if __name__ == "__main__":
+    app = MainLoop()
+    app.mainloop()
+
