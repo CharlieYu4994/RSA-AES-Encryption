9205882ca10b89939ea6212c44ad3380bb287516
CharlieYu4994 <charlieyu4994@outlook.com>
2020-07-13 22:10:52 +0800

    开始修改supports.py

---
+++ Codes/supports_dev.py
@@ -0,0 +1,60 @@
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Signature import pss
+from Crypto.Hash import SHA256
+import win32
+
+
+
+def pkcs7padding(_data, _block_size): # 使用 pkcs7 填充数据
+    padding_size = _block_size - len(_data) % _block_size
+    return _data + chr(padding_size).encode() * padding_size
+
+def pkcs7unpadding(_data): # 去填充
+    length = len(_data)
+    return _data[0:length - int(_data[-1])]
+
+def aes_encrypt(_key, _data):
+    cipher = AES.new(_key, AES.MODE_CBC, _key[0:16])
+    return cipher.encrypt(pkcs7padding(_data, AES.block_size))
+
+def aes_decrypt(_key, _data):
+    cipher = AES.new(_key, AES.MODE_CBC, _key[0:16])
+    return pkcs7unpadding(cipher.decrypt(_data), AES.block_size)
+
+def gen_rsakey(_length, _passphrase):
+    key = RSA.generate(_length)
+    return key.export_key(passphrase=_passphrase), key.publickey().export_key()
+
+def rsa_decrypt(_pubkey, _prikey, _data, _session_key):
+    cipher_rsa = PKCS1_OAEP.new(_prikey)
+    _session_key = cipher_rsa.decrypt(_session_key)
+    return aes_decrypt(_session_key, _data)
+
+def rsa_encrypt(_pubkey, _prikey, _data):
+    _session_key = get_random_bytes(16)
+    cipher_rsa = PKCS1_OAEP.new(_pubkey)
+    return cipher_rsa.encrypt(_session_key), aes_encrypt(_session_key, _data)
+    
+def pss_sign(_prikey, _data):
+    _hash = SHA256.new(_data)
+    return pss.new(_prikey).sign(_hash)
+
+def pss_verify(_pubkey, _data, _signature):
+    verifier = pss.new(_pubkey)
+    _hash = SHA256.new(_data)
+    try: verifier.verify(_hash, _signature); return True
+    except Exception as E: return False
+
+def set_text(text):
+    win32.win32clipboard.OpenClipboard()
+    win32.win32clipboard.EmptyClipboard()
+    win32.win32clipboard.SetClipboardData(win32.lib.win32con.CF_OEMTEXT, text)
+    win32.win32clipboard.CloseClipboard()
+
+if __name__ == "__main__":
+    recipient_key = RSA.import_key(open("receiver.pem").read())
+    private_key = RSA.import_key(open("private.pem").read())
+    massage = b'test'
+    
