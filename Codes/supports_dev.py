1db18d15b05aa67e34615170c2d4e12076beafd9
CharlieYu4994 <charlieyu4994@outlook.com>
2020-07-14 14:34:44 +0800

    加注释

--- Codes/supports_dev.py
+++ Codes/supports_dev.py
@@ -7,45 +7,48 @@ import win32
 
 
 
-def pkcs7padding(_data, _block_size): # 使用 pkcs7 填充数据
+# ------------------------------------BASIC Encrypt Part---------------------------------- #
+def pkcs7padding(_data: bytes, _block_size: int) -> bytes:
     padding_size = _block_size - len(_data) % _block_size
     return _data + chr(padding_size).encode() * padding_size
 
-def pkcs7unpadding(_data): # 去填充
+def pkcs7unpadding(_data: bytes) -> bytes: # 去填充
     length = len(_data)
     return _data[0:length - int(_data[-1])]
 
-def aes_encrypt(_key, _data):
+def aes_encrypt(_key: bytes, _data: bytes) -> bytes:
     cipher = AES.new(_key, AES.MODE_CBC, _key[0:16])
     return cipher.encrypt(pkcs7padding(_data, AES.block_size))
 
-def aes_decrypt(_key, _data):
+def aes_decrypt(_key: bytes, _data: bytes) -> bytes:
     cipher = AES.new(_key, AES.MODE_CBC, _key[0:16])
     return pkcs7unpadding(cipher.decrypt(_data), AES.block_size)
 
-def gen_rsakey(_length, _passphrase):
+def gen_rsakey(_length: int, _passphrase: int) -> bytes *2:
     key = RSA.generate(_length)
     return key.export_key(passphrase=_passphrase), key.publickey().export_key()
 
-def rsa_decrypt(_pubkey, _prikey, _data, _session_key):
+def rsa_decrypt(_pubkey, _prikey, _data: bytes, _session_key: bytes) -> bytes:
     cipher_rsa = PKCS1_OAEP.new(_prikey)
     _session_key = cipher_rsa.decrypt(_session_key)
     return aes_decrypt(_session_key, _data)
 
-def rsa_encrypt(_pubkey, _prikey, _data):
+def rsa_encrypt(_pubkey, _prikey, _data: bytes) -> bytes *2:
     _session_key = get_random_bytes(16)
     cipher_rsa = PKCS1_OAEP.new(_pubkey)
     return cipher_rsa.encrypt(_session_key), aes_encrypt(_session_key, _data)
     
-def pss_sign(_prikey, _data):
+def pss_sign(_prikey, _data: bytes) -> bytes:
     _hash = SHA256.new(_data)
     return pss.new(_prikey).sign(_hash)
 
-def pss_verify(_pubkey, _data, _signature):
+def pss_verify(_pubkey, _data: bytes, _signature: bytes) -> bool:
     verifier = pss.new(_pubkey)
     _hash = SHA256.new(_data)
     try: verifier.verify(_hash, _signature); return True
     except Exception as E: return False
+
+# 
 
 def set_text(text):
     win32.win32clipboard.OpenClipboard()
