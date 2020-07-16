e55f4ca4a768f84d9bb6a21e4e7a06965d3aaf3e
CharlieYu4994 <charlieyu4994@outlook.com>
2020-07-16 12:59:35 +0800

    子窗口

--- Codes/GUI_ver.py
+++ Codes/GUI_ver.py
@@ -2,14 +2,27 @@ import tkinter
 from tkinter import ttk
 from tkinter import scrolledtext
 
-class MainLoop(tkinter.Tk):
+
+class KeyManage(tkinter.Toplevel):
+    def __init__(self):
+        super().__init__()
+        self.title('KeyManager')
+        self.geometry('200x200')
+        self.resizable(0, 0)
+        self.setupUI
+
+    def setupUI(self):
+        pass
+
+
+class MainWindows(tkinter.Tk):
     keylist = [1]
 
     def __init__(self):
         super().__init__()
         self.title('RSA&AES Encryption')
         self.geometry('338x205')
-        self.resizable(0,0)
+        self.resizable(0, 0)
         self.setupUI()
 
     def setupUI(self):
@@ -22,7 +35,7 @@ class MainLoop(tkinter.Tk):
         inputbox.grid(column=0, row=0)
 
         footbox_page1 = ttk.Frame(frame0)
-        signcheck = ttk.Checkbutton(footbox_page1, text = "签名")
+        signcheck = ttk.Checkbutton(footbox_page1, text="签名")
         signcheck.grid(column=0, row=0, padx=20)
         encryptbtn_t = ttk.Button(footbox_page1, width=8, text='加密')
         encryptbtn_t.grid(column=1, row=0)
@@ -35,13 +48,13 @@ class MainLoop(tkinter.Tk):
         frame1 = ttk.Frame(tabs)
 
         dirbox = ttk.Frame(frame1)
-        dir_l_i = ttk.Label(dirbox,text='文件路径:')
+        dir_l_i = ttk.Label(dirbox, text='文件路径:')
         dir_l_i.grid(column=0, row=0)
         dir_e_i = ttk.Entry(dirbox, width=25)
         dir_e_i.grid(column=1, row=0)
         dir_b_i = ttk.Button(dirbox, text='选择文件', width=8)
         dir_b_i.grid(column=2, row=0)
-        dir_l_o = ttk.Label(dirbox,text='保存路径:')
+        dir_l_o = ttk.Label(dirbox, text='保存路径:')
         dir_l_o.grid(column=0, row=1)
         dir_e_o = ttk.Entry(dirbox, width=25)
         dir_e_o.grid(column=1, row=1)
@@ -53,7 +66,8 @@ class MainLoop(tkinter.Tk):
         prompt_bar = ttk.Label(footbox_page2, text='进度:')
         prompt_bar.grid(column=0, row=0, pady=5)
         progressbar = ttk.Progressbar(footbox_page2)
-        progressbar.grid(column=1, row=0, columnspan=19, sticky='ew', pady=5, padx=6)
+        progressbar.grid(column=1, row=0, columnspan=19,
+                         sticky='ew', pady=5, padx=6)
         encryptbtn_f = ttk.Button(footbox_page2, width=20, text='加密')
         encryptbtn_f.grid(column=0, columnspan=10, row=1, padx=5)
         decryptbtn_f = ttk.Button(footbox_page2, width=20, text='解密')
@@ -65,29 +79,41 @@ class MainLoop(tkinter.Tk):
         frame2 = ttk.Frame(tabs)
 
         footbox_page3 = ttk.Frame(frame2)
-        dir_l_cfg = ttk.Label(footbox_page3,text='服务器 URL:')
-        dir_l_cfg.grid(column=0, row=0)
-        dir_e_cfg = ttk.Entry(footbox_page3, width=32)
-        dir_e_cfg.grid(column=1, row=0)
-        footbox_page3.grid(column=0, row=0, columnspan=10, padx=16, pady=10)
+        url_l_cfg = ttk.Label(footbox_page3, text='服务器 URL:')
+        url_l_cfg.grid(column=0, row=0, pady=8)
+        url_e_cfg = ttk.Entry(footbox_page3, width=32)
+        url_e_cfg.grid(column=1, row=0, pady=8)
+        save_dir_l = ttk.Label(footbox_page3, text='保存路径    :')
+        save_dir_l.grid(column=0, row=1, pady=8)
+        save_dir_e = ttk.Entry(footbox_page3, width=32)
+        save_dir_e.grid(column=1, row=1, pady=8)
+        footbox_page3.grid(column=0, row=0, columnspan=10, padx=8, pady=20)
 
         save_btn = ttk.Button(frame2, width=8, text='保存')
         save_btn.grid(column=9, row=1)
 
+        btn_box = ttk.Frame(frame2)
+        pubkey_btn = ttk.Button(btn_box, width=8, text='导入公钥')
+        pubkey_btn.grid(column=0, row=0, padx=8)
+        prikey_btn = ttk.Button(btn_box, width=8, text='管理密钥', command=self.keymanage)
+        prikey_btn.grid(column=1, row=0, padx=8)
+        btn_box.grid(column=0, columnspan=9, row=1, pady=10)
+
         tabs.add(frame2, text="杂项")
 #--------------------------------------------标签栏------------------------------------------------#
         keybox = ttk.Frame(self)
-        keybox.grid(column=0, row=0, sticky = 'ne', padx=3, pady=1)
+        keybox.grid(column=0, row=0, sticky='ne', padx=3, pady=1)
         prompt = ttk.Label(keybox, text="收件人:")
-        prompt.grid(column=0, row=0, sticky = 'w')
+        prompt.grid(column=0, row=0, sticky='w')
         keyls = ttk.Combobox(keybox, width=10)
         keyls['values'] = self.keylist
         keyls.current(0)
         keyls.grid(column=1, row=0)
 
         tabs.grid(column=0, row=0)
+    def keymanage(self):
+        pass
 
 if __name__ == "__main__":
-    app = MainLoop()
+    app = MainWindows()
     app.mainloop()
-
--- Codes/supports_dev.py
+++ Codes/supports_dev.py
@@ -3,61 +3,96 @@ from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Signature import pss
 from Crypto.Hash import SHA256
-import win32
+import sqlite3
 
 
 
 # ------------------------------------BASIC Encrypt Part---------------------------------- #
 def pkcs7padding(_data: bytes, _block_size: int) -> bytes:
-    padding_size = _block_size - len(_data) % _block_size
-    return _data + chr(padding_size).encode() * padding_size
+    _padding_size = _block_size - len(_data) % _block_size
+    return _data + chr(_padding_size).encode() * _padding_size
 
-def pkcs7unpadding(_data: bytes) -> bytes: # 去填充
-    length = len(_data)
-    return _data[0:length - int(_data[-1])]
+
+def pkcs7unpadding(_data: bytes) -> bytes:  # 去填充
+    _length = len(_data)
+    return _data[0:_length - int(_data[-1])]
+
 
 def aes_encrypt(_key: bytes, _data: bytes) -> bytes:
-    cipher = AES.new(_key, AES.MODE_CBC, _key[0:16])
-    return cipher.encrypt(pkcs7padding(_data, AES.block_size))
+    _cipher = AES.new(_key, AES.MODE_CBC, _key[0:16])
+    return _cipher.encrypt(pkcs7padding(_data, AES.block_size))
+
 
 def aes_decrypt(_key: bytes, _data: bytes) -> bytes:
-    cipher = AES.new(_key, AES.MODE_CBC, _key[0:16])
-    return pkcs7unpadding(cipher.decrypt(_data), AES.block_size)
+    _cipher = AES.new(_key, AES.MODE_CBC, _key[0:16])
+    return pkcs7unpadding(_cipher.decrypt(_data), AES.block_size)
 
-def gen_rsakey(_length: int, _passphrase: int) -> bytes *2:
-    key = RSA.generate(_length)
-    return key.export_key(passphrase=_passphrase), key.publickey().export_key()
+
+def gen_rsakey(_length: int, _passphrase: str) -> bytes:
+    _key = RSA.generate(_length)
+    return _key.export_key(passphrase=_passphrase if _passphrase else None),\
+           _key.publickey().export_key()
+
 
 def rsa_decrypt(_pubkey, _prikey, _data: bytes, _session_key: bytes) -> bytes:
-    cipher_rsa = PKCS1_OAEP.new(_prikey)
-    _session_key = cipher_rsa.decrypt(_session_key)
+    _cipher = PKCS1_OAEP.new(_prikey)
+    _session_key = _cipher.decrypt(_session_key)
     return aes_decrypt(_session_key, _data)
 
-def rsa_encrypt(_pubkey, _prikey, _data: bytes) -> bytes *2:
+
+def rsa_encrypt(_pubkey, _prikey, _data: bytes) -> bytes:
     _session_key = get_random_bytes(16)
-    cipher_rsa = PKCS1_OAEP.new(_pubkey)
-    return cipher_rsa.encrypt(_session_key), aes_encrypt(_session_key, _data)
-    
+    _cipher = PKCS1_OAEP.new(_pubkey)
+    return _cipher.encrypt(_session_key), aes_encrypt(_session_key, _data)
+
+
 def pss_sign(_prikey, _data: bytes) -> bytes:
     _hash = SHA256.new(_data)
     return pss.new(_prikey).sign(_hash)
 
+
 def pss_verify(_pubkey, _data: bytes, _signature: bytes) -> bool:
-    verifier = pss.new(_pubkey)
+    _verifier = pss.new(_pubkey)
     _hash = SHA256.new(_data)
-    try: verifier.verify(_hash, _signature); return True
-    except Exception as E: return False
+    try:
+        _verifier.verify(_hash, _signature)
+        return True
+    except Exception as E:
+        return False
 
-# 
+# ---------------------------------------Database Part------------------------------------ #
 
-def set_text(text):
-    win32.win32clipboard.OpenClipboard()
-    win32.win32clipboard.EmptyClipboard()
-    win32.win32clipboard.SetClipboardData(win32.lib.win32con.CF_OEMTEXT, text)
-    win32.win32clipboard.CloseClipboard()
+
+def gen_database():
+    _db = sqlite3.connect('keys.db')
+    _cursor = _db.cursor()
+    _cursor.execute('''CREATE TABLE UserKeys(
+				ID           INTEGER PRIMARY KEY,
+				PubKey       TEXT    NOT NULL,
+				PriKey       TEXT    NOT NULL,
+				Describe     CHAR(30)         );''')
+    _cursor.execute('''CREATE TABLE ThirdKeys(
+				ID           INTEGER PRIMARY KEY,
+				PubKey       TEXT    NOT NULL,
+				Name         CHAR(10)NOT NULL );''')
+    _db.commit()
+    _db.close()
+
+def add_userkey(_pubkey: bytes, _prikey: bytes, _describe: str, _db):
+    _cursor = _db.cursor()
+    _cursor.execute(f"INSERT INTO UserKeys (PubKey, PriKey, describe) \
+			      VALUES ('{_pubkey.decode()}', '{_prikey.decode()}', '{_describe}')")
+    _db.commit()
+
+def del_key(_id: int, _table: str, _db):
+    _cursor = _db.cursor()
+    _cursor.execute(f"DELETE FROM '{_table}' WHERE id = {_id}")
+    _db.commit()
+ 
 
 if __name__ == "__main__":
-    recipient_key = RSA.import_key(open("receiver.pem").read())
-    private_key = RSA.import_key(open("private.pem").read())
-    massage = b'test'
-    
+    # gen_database()
+    database = sqlite3.connect('keys.db')
+    #prikey, _pubkey = gen_rsakey(3072, '')
+    #add_userkey(_pubkey, prikey, '测试', database)
+    del_key(1, 'UserKeys', database)
